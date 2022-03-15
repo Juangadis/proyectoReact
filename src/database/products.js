913704b03a5cdd8f352e953fc9712fc741c485e7
Juangadis <86635252+Juangadis@users.noreply.github.com>
2022-03-15 10:46:02 -0300

    Agregue una database con productos

---
+++ src/database/products.js
@@ -0,0 +1,163 @@
+const products = [{
+    "id": 1,
+    "title": "Juice - Grape, White",
+    "description": "Oth exc/dest intvrt disc",
+    "price": 147.92,
+    "category": "Baumbach, Dooley and Waelchi",
+    "imgUrl": "http://dummyimage.com/195x100.png/5fa2dd/ffffff",
+    "stock": 15
+  }, {
+    "id": 2,
+    "title": "Muffin Chocolate Individual Wrap",
+    "description": "Intravas msmt periph art",
+    "price": 260.3,
+    "category": "Schuppe, Green and Rogahn",
+    "imgUrl": "http://dummyimage.com/169x100.png/5fa2dd/ffffff",
+    "stock": 11
+  }, {
+    "id": 3,
+    "title": "Longos - Chicken Wings",
+    "description": "Intraop gastric manipul",
+    "price": 454.1,
+    "category": "Kirlin-Johnson",
+    "imgUrl": "http://dummyimage.com/101x100.png/cc0000/ffffff",
+    "stock": 10
+  }, {
+    "id": 4,
+    "title": "Cheese - Montery Jack",
+    "description": "Arth/pros rem wo re-foot",
+    "price": 181.69,
+    "category": "Kuhic-Mueller",
+    "imgUrl": "http://dummyimage.com/151x100.png/5fa2dd/ffffff",
+    "stock": 17
+  }, {
+    "id": 5,
+    "title": "Pasta - Canelloni, Single Serve",
+    "description": "Closed brain biopsy",
+    "price": 128.62,
+    "category": "Maggio Inc",
+    "imgUrl": "http://dummyimage.com/231x100.png/dddddd/000000",
+    "stock": 7
+  }, {
+    "id": 6,
+    "title": "Ice Cream - Life Savers",
+    "description": "Tendon trnsfr/transplant",
+    "price": 276.79,
+    "category": "Zboncak, Bins and Jerde",
+    "imgUrl": "http://dummyimage.com/225x100.png/5fa2dd/ffffff",
+    "stock": 5
+  }, {
+    "id": 7,
+    "title": "Energy Drink Red Bull",
+    "description": "Lap mul seg res lg intes",
+    "price": 296.17,
+    "category": "Deckow, Gaylord and Okuneva",
+    "imgUrl": "http://dummyimage.com/129x100.png/cc0000/ffffff",
+    "stock": 16
+  }, {
+    "id": 8,
+    "title": "Appetizer - Sausage Rolls",
+    "description": "Endosc remove panc stone",
+    "price": 278.39,
+    "category": "Cole Group",
+    "imgUrl": "http://dummyimage.com/245x100.png/5fa2dd/ffffff",
+    "stock": 7
+  }, {
+    "id": 9,
+    "title": "Beer - Heinekin",
+    "description": "Corneal repair NEC",
+    "price": 453.15,
+    "category": "Rau, VonRueden and Volkman",
+    "imgUrl": "http://dummyimage.com/183x100.png/dddddd/000000",
+    "stock": 8
+  }, {
+    "id": 10,
+    "title": "Sugar - Brown",
+    "description": "Total patellectomy",
+    "price": 135.66,
+    "category": "VonRueden-Huels",
+    "imgUrl": "http://dummyimage.com/119x100.png/5fa2dd/ffffff",
+    "stock": 13
+  }, {
+    "id": 11,
+    "title": "Veal - Insides, Grains",
+    "description": "Rt heart cardiac cath",
+    "price": 400.91,
+    "category": "Bernier-Gleichner",
+    "imgUrl": "http://dummyimage.com/168x100.png/ff4444/ffffff",
+    "stock": 13
+  }, {
+    "id": 12,
+    "title": "Sorrel - Fresh",
+    "description": "Puncture of spleen",
+    "price": 494.37,
+    "category": "Dickens, Wisozk and Huel",
+    "imgUrl": "http://dummyimage.com/145x100.png/5fa2dd/ffffff",
+    "stock": 18
+  }, {
+    "id": 13,
+    "title": "Beef - Rib Roast, Capless",
+    "description": "Bone grft to metacar/car",
+    "price": 120.98,
+    "category": "Feil-Herman",
+    "imgUrl": "http://dummyimage.com/167x100.png/dddddd/000000",
+    "stock": 17
+  }, {
+    "id": 14,
+    "title": "Eggroll",
+    "description": "Abdperneal res rectm NOS",
+    "price": 446.23,
+    "category": "Hudson, Herzog and Simonis",
+    "imgUrl": "http://dummyimage.com/103x100.png/ff4444/ffffff",
+    "stock": 5
+  }, {
+    "id": 15,
+    "title": "Cake Slab",
+    "description": "Subtotal mastectomy",
+    "price": 183.98,
+    "category": "Bruen Group",
+    "imgUrl": "http://dummyimage.com/101x100.png/5fa2dd/ffffff",
+    "stock": 5
+  }, {
+    "id": 16,
+    "title": "Temperature Recording Station",
+    "description": "Peripheral nerve graft",
+    "price": 481.57,
+    "category": "Schmeler, Koepp and Jacobson",
+    "imgUrl": "http://dummyimage.com/237x100.png/dddddd/000000",
+    "stock": 18
+  }, {
+    "id": 17,
+    "title": "Bread Ww Cluster",
+    "description": "Percutaneous cystostomy",
+    "price": 299.28,
+    "category": "Kunze, Greenholt and Zulauf",
+    "imgUrl": "http://dummyimage.com/182x100.png/dddddd/000000",
+    "stock": 20
+  }, {
+    "id": 18,
+    "title": "Longos - Chicken Caeser Salad",
+    "description": "Excis perianal skin tag",
+    "price": 108.64,
+    "category": "Pacocha-Rice",
+    "imgUrl": "http://dummyimage.com/229x100.png/dddddd/000000",
+    "stock": 16
+  }, {
+    "id": 19,
+    "title": "Longos - Chicken Curried",
+    "description": "Urethral pressure profil",
+    "price": 326.17,
+    "category": "Botsford Group",
+    "imgUrl": "http://dummyimage.com/147x100.png/cc0000/ffffff",
+    "stock": 13
+  }, {
+    "id": 20,
+    "title": "Coriander - Seed",
+    "description": "Other heat therapy",
+    "price": 154.08,
+    "category": "Yost-Bayer",
+    "imgUrl": "http://dummyimage.com/223x100.png/5fa2dd/ffffff",
+    "stock": 17
+  }]
+
+  export default products;
